628b8471cb3926e2332e7b2c62c41a832831250d
broeschi <39580893+broeschi@users.noreply.github.com>
2018-06-28 08:49:44 +0200

    2. Versuch zum korrekten Aufbau

--- src/GUI/PersonController.java
+++ src/GUI/PersonController.java
@@ -9,9 +9,9 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import person.Person;
+import Person.Person;
 //import ch.makery.adress.util.DateUtil;
-import person.Person;
+
 
 /**
  * Detailinfo der Personen bearbeiten
