6a9546b947260cdf60e93ebc3c87baef9deb3465
broeschi <39580893+broeschi@users.noreply.github.com>
2018-07-02 06:32:49 +0200

    30.06.2018

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -1,6 +1,12 @@
 package Person;
 
 import java.util.Date;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 /** Klasse zum Bilden der Teilnehmer*/
 public class Person {
